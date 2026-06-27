0643d2388fbbfd5dc72372c1ebfbb0f1e330e593
anuj paul <paulanuj27@gmail.com>
2026-06-27 11:32:09 +0600

    Create a room for one-to-one chat and receive message and send message to the with roomId

--- src/services/socket.services.js
+++ src/services/socket.services.js
@@ -1,4 +1,5 @@
 const { Server } = require("socket.io");
+const saveMessage = require("../controllers/message.controller");
 
 const initSocket = (server) => {
   const io = new Server(server, {
@@ -9,10 +10,33 @@ const initSocket = (server) => {
   });
 
   io.on("connection", (socket) => {
-
     console.log("User connected via services: 👤", socket.id);
 
+    // 1. Create room for individual  
+    socket.on('joinRoom', async ({senderId, receiverId})=> {
+        // create id of this room 
+        const roomId = [senderId,receiverId].sort().join('-');
+        
+        // socket go the this room 
+        socket.join(roomId)
+        console.log(`User ${socket.id} joined room: 🔑 ${roomId}`);
+    })
 
+    // 2.Room wise message sending event 
+    socket.on('sendMessage', async(data)=> {
+        try {
+            const {sender, receiver} = data;
+            const roomId = [sender,receiver].sort().join('-')
+
+            // message save to the DB 
+            const saveMsg = await saveMessage(data)
+            io.to(roomId).emit('receiveMessage', saveMsg)
+        }
+        catch (err)
+        {
+            socket.emit('error', 'message send failed')
+        }
+    })
     socket.on('disconnect', ()=> {
         console.log('User disconnected ❌');
     })
